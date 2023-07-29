df15837a34f4a587794f17f1f64105eb8c486434
Niko Ivan <nixonivan@rambler.ru>
2023-07-29 12:53:36 +0300

    secons task done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Collapse from "./components/Collapse";
+import Chat from "./components/Chat";
 
 const collapse = new Collapse("wrapper");
-
 collapse.init();
+
+const chat = new Chat("container");
